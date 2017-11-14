0fe8cacbc0c1c7320ca37d7b36bad7056c989a4a
Pedro Castaneda <itspedrocastaneda@gmail.com>
2017-11-14 15:03:51 -0800

    add html sections to site

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ particlesJS('particles-js', {
       }
     },
     "color": {
-      "value": ["#39393A", "#C3F73A", "#FC440F", "#2EC4B6"]
+      "value": ["#121416", "#C3F73A", "#FC440F", "#2EC4B6"]
     },
     "shape": {
       "type": "circle",
